2b516b9e1cb8d91400b2f3df6cfe90d49d38d97e
leanayemeneenlilia <leana.yemene@enlilia.com>
2024-11-06 15:27:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Create from './components/Create';
+import NavBar from './components/NavBar';
 
 function App() {
+  const myWidth = 220
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar 
+          drawerWidth = {myWidth}
+          content = {
+            <Routes>
+              <Route path='' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/create' element={<Create/>}/>
+            </Routes>
+          }
+      
+      />
+
+
     </div>
   );
 }
