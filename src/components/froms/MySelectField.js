6511df98c383eebae755ee037e0ed5de1acba23a
leanayemeneenlilia <leana.yemene@enlilia.com>
2024-11-06 15:54:07 +0000

    Add files via upload

---
+++ src/components/froms/MySelectField.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import {Controller} from 'react-hook-form'
+
+export default function MySelectField(props) {
+  const{label,width, name , control} = props
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+
+
+      <FormControl variant="standard" sx ={{width:{width}}}>
+      <InputLabel id="demo-simple-select-filled-label">{label}</InputLabel>
+            <Controller 
+                    name = {name}
+                    control = {control}
+
+                    render ={({
+                        field : {onCange, value},
+                        fieldState: {error},
+                        formState,
+
+                    }) => (     
+                        <Select
+                            labelId="demo-simple-select-filled-label"
+                            id="demo-simple-select-filled"
+                            value={age}
+                            onChange={handleChange}
+                            >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Open</MenuItem>
+                            <MenuItem value={20}>In Progress</MenuItem>
+                            <MenuItem value={30}>Completed</MenuItem>
+                        </Select>)
+                    
+            }
+                />
+        
+        
+      </FormControl>
+  );
+}
