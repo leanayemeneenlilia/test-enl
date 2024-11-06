c1d8242261cc1e2428196579ac4c123dd44c0354
leanayemeneenlilia <leana.yemene@enlilia.com>
2024-11-06 17:03:48 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-
 import './App.css';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
 import NavBar from './components/NavBar';
 
 function App() {
-  const myWidth = 220
+  const myWidth = 220;
+
+  // Ajouter un log pour vérifier si le composant App se rend correctement
+  console.log("App component is rendering...");
+
   return (
     <div className="App">
+      {/* Log pour vérifier si la barre de navigation est rendue */}
+      {console.log("Rendering NavBar with content...")}
+
       <NavBar 
-          drawerWidth = {myWidth}
-          content = {
-            <Routes>
-              <Route path='' element={<Home/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/create' element={<Create/>}/>
-            </Routes>
-          }
-      
+        drawerWidth={myWidth}
+        content={
+          <Routes>
+            <Route path='' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/create' element={<Create />} />
+          </Routes>
+        }
       />
-
-
     </div>
   );
 }
