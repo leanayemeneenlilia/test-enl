3cce8ab262ef15297c99f5f248b52fd9c2faf082
leanayemeneenlilia <leana.yemene@enlilia.com>
2024-11-07 08:59:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,24 +8,19 @@ import NavBar from './components/NavBar';
 function App() {
   const myWidth = 220;
 
-  // Ajouter un log pour vérifier si le composant App se rend correctement
   console.log("App component is rendering...");
 
   return (
     <div className="App">
-      {/* Log pour vérifier si la barre de navigation est rendue */}
       {console.log("Rendering NavBar with content...")}
 
-      <NavBar 
-        drawerWidth={myWidth}
-        content={
-          <Routes>
-            <Route path='' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/create' element={<Create />} />
-          </Routes>
-        }
-      />
+      <NavBar drawerWidth={myWidth} />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
